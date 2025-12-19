d237bb4618dfb0485deddc9d30e70bfcf89f8ff2
Chang-Jin-Lee <ckdwls525@gmail.com>
2025-12-19 22:56:28 +0900

    [feat] Cmake installer 주석처리 및 버전 맞추기

--- CMake/installer.cmake
+++ CMake/installer.cmake
@@ -27,7 +27,7 @@
 
 # first step set up all necessary variables
 if(WIN32 AND NOT UNIX)
-    if (NOT CMAKE_VERSION VERSION_LESS 3.1.0)
+    if (NOT CMAKE_VERSION VERSION_LESS 3.20)
         set(CPACK_GENERATOR 7Z)
     else()
         set(CPACK_GENERATOR ZIP)
